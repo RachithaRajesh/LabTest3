826757749757ffe0e09034e795f355a278efee35
sanjana193 <sanjanaayadav319@gmail.com>
2025-03-17 15:14:32 +0530

    First commit

---
+++ Area.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Area{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		r=sc.nxtInt();
+		int r;
+		int CircleArea;
+		CircleArea=(3.14)*r*r;
+		System.out.println("area"+CircleArea);
+	}
+}
