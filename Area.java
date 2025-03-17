ba90f6f706153b7ee59700171e7e99863d1e72b7
RachithaRajesh <rachitharajesh.22@gmail.com>
2025-03-17 15:21:07 +0530

    Area.java

--- Area.java
+++ Area.java
@@ -3,9 +3,9 @@ public class Area{
 	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
-		r=sc.nxtInt();
+		r=sc.nextInt();
 		int r;
-		int CircleArea;
+		double CircleArea;
 		CircleArea=(3.14)*r*r;
 		System.out.println("area"+CircleArea);
 	}
